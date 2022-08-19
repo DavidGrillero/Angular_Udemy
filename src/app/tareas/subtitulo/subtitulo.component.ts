7d46e5816b5d5689983b925145d7c7cdf9e666e4
David Grillo <dmesar@correo.iue.edu.co>
2022-08-18 21:31:15 -0500

    base proyecto

---
+++ src/app/tareas/subtitulo/subtitulo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subtitulo',
+  templateUrl: './subtitulo.component.html',
+  styleUrls: ['./subtitulo.component.css']
+})
+export class SubtituloComponent {
+
+
+}
